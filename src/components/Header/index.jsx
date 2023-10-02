33ea4b9f0bc57c145fcb1857ddff8f209cd1079c
Sycrusian <sycrus@protonmail.com>
2023-10-02 16:45:42 -0300

    Minor adjustment

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,29 +1,29 @@
 import styles from "./style.module.scss";
-// import { regularCursor, waitCursor } from "../../services/cursor";
-// import { Button } from "../widgets/Button";
-// import { blogApi } from "../../services/api";
+import { regularCursor, waitCursor } from "../../services/cursor";
+import { Button } from "../widgets/Button";
+import { blogApi } from "../../services/api";
 
 export const Header = ({setNewsList}) => {
-  // const getNews = async () => {
-  //   try {
-  //     waitCursor();
-  //     const { data } = await blogApi.get("/news");
-  //     setNewsList(data);
-  //   }
-  //   catch(error) {
-  //     console.error(error.message);
-  //   }
-  //   finally {
-  //     regularCursor();
-  //   }
-  // }
+  const getNews = async () => {
+    try {
+      waitCursor();
+      const { data } = await blogApi.get("/news");
+      setNewsList(data);
+    }
+    catch(error) {
+      console.error(error.message);
+    }
+    finally {
+      regularCursor();
+    }
+  }
 
   return (
     <header className={styles.header}>
       <div>
         <h1>* Notícias *</h1>
       </div>
-      {/* <Button click={getNews}>Carregar</Button> */}
+      <Button click={getNews}>Carregar</Button>
     </header>
   );
 }
