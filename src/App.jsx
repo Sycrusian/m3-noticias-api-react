a2e23d0bf75bcd636f7b60e218b48666e33b18b3
Sycrusian <sycrus@protonmail.com>
2023-10-02 16:41:18 -0300

    Atividade | Executando uma requisição com useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import "./styles/index.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { blogApi } from "./services/api";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 
 const App = () => {
   const [ newsList, setNewsList ] = useState([]);
+
+  useEffect(() => { getNews() }, []);
+
+  const getNews = async () => {
+    try {
+      const { data } = await blogApi.get("/news");
+      setNewsList(data);
+    }
+    catch(error) {
+      console.error(error.message);
+    }
+  }
 
   return (
     <>
